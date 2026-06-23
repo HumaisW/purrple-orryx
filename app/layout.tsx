50bc806da545ff9dcb05c3ba4bd9a72a5a67aee4
Humais <Humais@purrpleorryx.com>
2026-06-23 13:35:48 +0500

    Install Google Tag Manager container

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Outfit, Playfair_Display } from "next/font/google";
 import "./globals.css";
-
+import Script from "next/script";
 const outfit = Outfit({
   variable: "--font-outfit",
   subsets: ["latin"],
@@ -67,14 +67,46 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html
-      lang="en"
-      className={`${outfit.variable} ${playfair.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col font-sans bg-[var(--color-background)] text-[var(--color-foreground)]">
-        {children}
-      </body>
-    </html>
-  );
-}
+ return (
+  <html
+    lang="en"
+    className={`${outfit.variable} ${playfair.variable} h-full antialiased`}
+  >
+    <body className="min-h-full flex flex-col font-sans bg-[var(--color-background)] text-[var(--color-foreground)]">
+      {/* Google Tag Manager noscript */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-MM74MX"
+          height="0"
+          width="0"
+          style={{
+            display: "none",
+            visibility: "hidden",
+          }}
+        />
+      </noscript>
+
+      {children}
+
+      {/* Google Tag Manager */}
+      <Script id="google-tag-manager" strategy="beforeInteractive">
+        {`
+          (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({
+              'gtm.start': new Date().getTime(),
+              event:'gtm.js'
+            });
+            var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),
+                dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MM74MX');
+        `}
+      </Script>
+    </body>
+  </html>
+);
+  }
