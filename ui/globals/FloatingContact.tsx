1fa7bee7376bc58ffea22f3e9fc0f1c3eb5294d5
Humais <Humais@purrpleorryx.com>
2026-07-21 21:01:59 +0500

    Update FloatingContact.tsx

--- ui/globals/FloatingContact.tsx
+++ ui/globals/FloatingContact.tsx
@@ -18,7 +18,7 @@ export default function FloatingContact() {
       
       {/* WhatsApp Button */}
       <a 
-        href="https://wa.me/971551035775" 
+        href="https://wa.me/97148842588"
         target="_blank" 
         rel="noopener noreferrer"
         className="w-14 h-14 bg-[#25D366] text-white rounded-full flex items-center justify-center shadow-[0_10px_30px_rgba(37,211,102,0.3)] hover:shadow-[0_10px_30px_rgba(37,211,102,0.5)] hover:-translate-y-1 transition-all duration-300 group"
